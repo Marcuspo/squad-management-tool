e3c6d14f67f9f75337f18a5247b85ee5346b67a0
Marcus Pontes <marcusporama@gmail.com>
2020-11-02 14:15:22 -0300

    Fix

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,5 @@
-import { Card, Button } from "react-bootstrap"
+import React from "react"
+import { Card, Button, Dropdown } from "react-bootstrap"
 
 import { VscAdd } from "react-icons/vsc"
 
@@ -11,11 +12,12 @@ function Main() {
         <Card.Body>
           <Card.Title className="card-title ">
             My teams{" "}
-            <Button className="btn float-right button-card">
-              <VscAdd />
+            <Button className="btn btn-light float-right button-card">
+              <VscAdd color="white" />
             </Button>
           </Card.Title>
         </Card.Body>
+        <Dropdown.Divider />
         <Card.Body>
           <Card.Text className="card-name">
             Some quick example text to build on the card title and make up the

a0a4ddbb78363b320152b38bfa830daf9dbd7a2b
Marcus Pontes <marcusporama@gmail.com>
2020-11-02 15:34:05 -0300

    ajustes do lado esquerdo do quadro

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,15 @@
 import React from "react"
 import { Card, Button, Dropdown } from "react-bootstrap"
 
+import FormControl from "@material-ui/core/FormControl"
+import InputLabel from "@material-ui/core/InputLabel"
+import Select from "@material-ui/core/Select"
+import MenuItem from "@material-ui/core/MenuItem"
+
 import { VscAdd } from "react-icons/vsc"
 
 import "./Main.css"
+import { Grid, TextField } from "@material-ui/core"
 
 function Main() {
   return (
@@ -19,22 +25,60 @@ function Main() {
         </Card.Body>
         <Dropdown.Divider />
 
-        <Card.Body>
-          <Dropdown>
-            <Dropdown.Toggle variant="light">Dropdown Button</Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-
-          <Card.Text className="card-name">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
+        <Grid container spacing={1} className="grid-item-1">
+          <Grid item xs={6} container>
+            <Card.Body>
+              <FormControl style={{ minWidth: 300 }}>
+                <InputLabel>Nome</InputLabel>
+                <Select
+                  labelId="demo-simple-select-autowidth-label"
+                  id="demo-simple-select-autowidth"
+                >
+                  <MenuItem value={1}>Nome 1</MenuItem>
+                  <MenuItem value={2}>Nome 2</MenuItem>
+                  <MenuItem value={3}>Nome 3</MenuItem>
+                </Select>
+              </FormControl>
+              <Card.Text className="card-name">Barcelona</Card.Text>
+              <Dropdown.Divider />
+              <Card.Text className="card-name">Real Madrid</Card.Text>
+              <Dropdown.Divider />
+              <Card.Text className="card-name">Milan</Card.Text>
+              <Dropdown.Divider />
+              <Card.Text className="card-name">Liverpool</Card.Text>
+              <Dropdown.Divider />
+              <Card.Text className="card-name">Bayern Munich</Card.Text>
+              <Dropdown.Divider />
+              <Card.Text className="card-name">Lazio</Card.Text>
+            </Card.Body>
+          </Grid>
+          <Grid container item xs={6}>
+            <Card.Body>
+              <FormControl style={{ minWidth: 250 }}>
+                <InputLabel>Description</InputLabel>
+                <Select
+                  labelId="demo-simple-select-autowidth-label"
+                  id="demo-simple-select-autowidth"
+                >
+                  <MenuItem value={1}>Nome 1</MenuItem>
+                  <MenuItem value={2}>Nome 2</MenuItem>
+                  <MenuItem value={3}>Nome 3</MenuItem>
+                </Select>
+              </FormControl>
+              <Card.Text className="card-name">Barcelona</Card.Text>
+              <Dropdown.Divider />
+              <Card.Text className="card-name">Real Madrid</Card.Text>
+              <Dropdown.Divider />
+              <Card.Text className="card-name">Milan</Card.Text>
+              <Dropdown.Divider />
+              <Card.Text className="card-name">Liverpool</Card.Text>
+              <Dropdown.Divider />
+              <Card.Text className="card-name">Bayern Munich</Card.Text>
+              <Dropdown.Divider />
+              <Card.Text className="card-name">Lazio</Card.Text>
+            </Card.Body>
+          </Grid>
+        </Grid>
       </Card>
     </div>
   )
